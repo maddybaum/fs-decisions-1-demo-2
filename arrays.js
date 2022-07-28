ab6c3f7a6582eae86833404722e8e43ba95355c2
maddybaum <madeline.baum94@gmail.com>
2022-07-28 14:25:01 -0500

    initial commit

--- arrays.js
+++ arrays.js
@@ -61,3 +61,24 @@ let longest;
 for (let i = 0; i <= 3; i++){
     console.log(backpack2[i])
 }
+//What if you ran the above for-loop, but did not have 3 items in the array? We could add a level of safety to it, by wrapping it in an if-else statement.//
+
+let guessMe = 54;
+
+while (guessMe < 100) {
+  console.log("----------");
+
+  if (guessMe % 4 == 0 || guessMe % 5 == 0) {
+    console.log("guessMe is divisible by 4 or 5.  Added 25.");
+    guessMe += 25;
+  } else if (guessMe % 3 == 0) {
+    console.log("guessMe is divisible by 3.  Subtracted 27.");
+    guessMe -= 27;
+  } else {
+    console.log("Added 3.");
+    guessMe += 3;
+  }
+
+  guessMe += 22;
+  console.log(`guessMe is now ${guessMe}.`);
+}
